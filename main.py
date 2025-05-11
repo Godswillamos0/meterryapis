72c1148829a3e1d064d203d2389b8f8f7874bc73
Godswill Amos <amosgodswill00@gmail.com>
2025-05-11 16:13:38 +0100

    Update main.py
    A fix to add CORS Support on Your FastAPI App

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import models
 from routers import smart_meter, esp32, users
 from database import engine
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://meterry.netlify.app"],  # Your frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
