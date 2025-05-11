7eb11b9210da5c9ba9ba71ff9cfc6663f29e9f14
Godswill Amos <amosgodswill00@gmail.com>
2025-05-11 16:32:33 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://meterry.netlify.app"],  # Your frontend domain
+    allow_origins=["*"],  # Your frontend domain
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
