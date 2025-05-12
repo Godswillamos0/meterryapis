08f2ff2944787d5a316592e37e41c62f9392569d
Godswill Amos <amosgodswill00@gmail.com>
2025-05-13 00:59:10 +0100

    Update database.py

--- database.py
+++ database.py
@@ -4,8 +4,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./data.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread':False})
-
+engine = create_engine(SQLALCHEMY_DATABASE_URL, 
+                      connect_args={'check_same_thread':False},
+                      pool_size=10,        # Default is 5; Increase based on your needs
+                      max_overflow=20,     # Default is 10; Controls how many extra connections can be created
+                      pool_timeout=30,     # Default is 30 seconds
+                      pool_recycle=1800    # Recycle connections every 30 minutes to avoid stale connection)
+                      )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
