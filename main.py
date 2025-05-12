f2778328779f55588efca7d09bc85ee2b294b0cc
Godswill Amos <amosgodswill00@gmail.com>
2025-05-12 22:06:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[*],            # or ["*"] to allow all (not recommended for production)
+    allow_origins=["*"],            # or ["*"] to allow all (not recommended for production)
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
